92e8b458baa788ea3f086e02f9c50ca1c1131601
Gustavo <gustavomorenodaveiga@live.com>
2019-04-17 15:42:03 -0300

    cpp: task 2

---
+++ cpp_src/sources/task2.cpp
@@ -0,0 +1,47 @@
+#include <lesson/queue.h>
+#include <lesson/stack.h>
+#include <lesson/vector.h>
+#include <iostream>
+
+int main() {
+  // Fila
+  auto fila = lesson::queue(10);
+  fila.push(234);
+  fila.push(646);
+  fila.push(195);
+  fila.push(745);
+  fila.push(864);
+  fila.push(246);
+
+  // Pilha
+  auto pilha = lesson::stack(10);
+  pilha.push(11);
+  pilha.push(56);
+  pilha.push(96);
+  pilha.push(28);
+  pilha.push(58);
+  pilha.push(24);
+  pilha.push(96);
+  pilha.push(26);
+
+  // Vector
+  auto vetor = lesson::vector(20);
+  while (!fila.empty() || !pilha.empty()) {
+    if (!fila.empty()) {
+      vetor.add(fila.pop());
+    }
+    if (!pilha.empty()) {
+      vetor.add(pilha.pop());
+    }
+  }
+
+  for (int i = 0; i < vetor.end(); i++) {
+    std::cout << vetor.get(i) << " ";
+  }
+
+  std::cout << std::endl;
+  std::cout << "Pressione ENTER para finalizar a programa... ";
+  std::cin.get();
+
+  return EXIT_SUCCESS;
+}
