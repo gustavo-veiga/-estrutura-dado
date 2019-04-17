364e3f39999d8538da2442aac2fc231992d56d21
Gustavo <gustavomorenodaveiga@live.com>
2019-04-17 13:07:04 -0300

    cpp: update console

--- cpp_src/includes/lesson/console.cc
+++ cpp_src/includes/lesson/console.cc
@@ -7,32 +7,41 @@
 #endif
 
 #ifdef _WIN32
+
 int lesson::console::rows() {
   CONSOLE_SCREEN_BUFFER_INFO csbi;
   GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
   return csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
 }
+
 int lesson::console::columns() {
   CONSOLE_SCREEN_BUFFER_INFO csbi;
   GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
   return csbi.srWindow.Right - csbi.srWindow.Left + 1;
 }
+
 #elif __linux__
+
 int lesson::console::row() {
   struct winsize size;
   ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
   return size.ws_row;
 }
+
 int lesson::console::columns() {
   struct winsize size;
   ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
   return size.ws_col;
 }
+
 #else
+
 int lesson::console::row() {
   return 0;
 }
+
 int lesson::console::columns() {
   return 0;
 }
+
 #endif
