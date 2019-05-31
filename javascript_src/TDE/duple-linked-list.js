da594b0895916ef14f17f4230eb0d512527ee8a8
teckthor <ademarjunior@unigranrio.br>
2019-05-31 13:11:34 -0300

    Implementando TDE

--- javascript_src/TDE/duple-linked-list.js
+++ javascript_src/TDE/duple-linked-list.js
@@ -3,13 +3,97 @@ class LinkedNode {
     constructor(value) {
         this.value = value
         this.nextNode = null
-        this.previous = null
+        this.previousNode = null
     }
 }
 
 class LinkedList {
     constructor(props) {
         this.initialNode = null
+    }
+
+    isEmpty() {
+        return this.initialNode === null
+    }
+
+    hasOnlyElement() {
+        if (this.isEmpty()) {
+            return false
+        }
+
+        return this.initialNode.nextNode === this.initialNode
+    }
+
+    showList() {
+        if (this.isEmpty()) {
+            return
+        }
+
+        let node = this.initialNode
+        do {
+            console.log(`=>>>> Player: ${node.value}`)
+            node = this.node.nextNode
+        }
+        while (node.nextNode !== this.initialNode)
+    }
+
+    push(value) {
+
+        if (this.isEmpty()) {
+            const node = new LinkedNode(value)
+            node.nextNode = node
+            node.previousNode = node
+            this.initialNode = node
+            return node
+        }
+
+        if (this.hasOnlyElement()) {
+            const currentNode = new LinkedNode(value)
+            currentNode.nextNode = this.initialNode.previousNode
+            this.initialNode.nextNode = currentNode.previousNode
+
+            return currentNode
+
+        }
+        const currentNode = new LinkedNode(value)
+        currentNode.nextNode = this.initialNode.nextNode
+        this.initialNode.nextNode = currentNode.previousNode
+
+        return currentNode
+
+    }
+
+    removeNode(nodeToRemove) {
+        if (this.isEmpty()) {
+            return null
+        }
+
+        console.log('=>>> Como está a lista?')
+        this.showList()
+
+        console.log(`=>>>> Removendo o player ${nodeToRemove.value}`)
+
+        if (this.initialNode.value === nodeToRemove.value) {
+            const lastElement = this.getLastElement()
+            lastElement.nextNode = this.initialNode.nextNode
+            this.initialNode = this.initialNode.nextNode
+
+            return nodeToRemove
+        }
+
+        let node = this.initialNode
+        let found = null
+        do {
+            if (node.nextNode.value == nodeToRemove.value) {
+                found = { ...node.nextNode }
+                node.nextNode = found.nextNode
+                break
+            }
+
+            node = node.nextNode
+        } while (node.nextNode != this.initialNode)
+
+        return found
     }
 
 }
--- javascript_src/TDE/index.js
+++ javascript_src/TDE/index.js
@@ -8,6 +8,8 @@ const { LinkedList } = require('./duple-linked-list')
     3 – Elimina o terceiro jogador contado a partir do jogador atual.
     9 – Elimina o jogador anterior na roda.
     12 – Inverte o sentido do jogo.
+    0 - Jogador atual fica 3 rodadas sem jogar
+    13 - 
  */
 
 const getRandomNumber = () => {
@@ -29,19 +31,151 @@ const getRandomNumber = () => {
         return 12
     }
 
+    /*
+    else if (randomValue == 5) {
+        return 0
+    }
+
+    else if (randomValue == 6) {
+        return 13
+    }
+    */
 }
 
+
+const executeToOneNumber = (currentPlayer) => {
+    console.log('=>> Pula o próximo jogador e passa a vez para o seguinte')
+    return currentPlayer.nextNode.nextNode
+}
+
+const executeToThreeNumber = (list, currentPlayer) => {
+    const playerToRemove = currentPlayer.nextNode.nextNode
+    console.log('=>> Elimina o terceiro jogador contado a partir do jogador atual.')
+    list.removeNode(playerToRemove)
+    return currentPlayer.nextNode
+}
+
+const executeToNineNumber = (list, currentPlayer, lastPlayer) => {
+    console.log('=>> Elimina o jogador da rodada anterior (o jogador que tirou a carta antes).')
+    list.removeNode(lastPlayer)
+    return currentPlayer.nextNode
+}
+
+const executeToTwelveNumber = () => {
+    console.log('Inverte o sentido do jogo')
+}
+
+const executeToZeroNumber = (currentPlayer) => {
+    console.log(`O jogador ${currentPlayer} ficara 3 rodadas sem poder jogar`)
+}
+
+const executeToThirteenNumber = () => {
+    console.log('Escolha quantos jogadores deseja pular: ')
+}
+
+
+
+
+
 const main = () => {
+    console.clear()
     console.log('=> Começando desafio...')
 
     console.log('=> Inicializando a lista')
-    const linkedList = new LinkedList()
+        const linkedList = new LinkedList()
 
     console.log('=> Insira a lista de players...\n')
 
-    while (true) {
-        let players = []
-
-        players = readline.question(`Digite o nome do player ${indice}: `)
+    let players = []
+    for (let i = 0; i < 10; i++) {
+        players[i] = readline.question(`Digite o nome do player ${i + 1}: `)
     }
+
+    // Carregando a lista duplamente encadeada
+    players.forEach(player => {
+        linkedList.push(player)
+    })
+
+    console.log('=> A lista foi populada com sucesso')
+        // console.clear()
+        // linkedList.showList()
+    
+
+console.log('=> Começando as rodadas...\n')
+let winner = null
+let round = 1
+let currentPlayer = { ...linkedList.initialNode }
+let lastPlayer = { ...linkedList.initialNode }
+let lastNumber = null
+
+while (winner === null) {
+    // Já encontramos um vencedor
+    if (linkedList.hasOnlyElement()) {
+        console.log('\n=>> Chegamos no primeiro elemento')
+        winner = linkedList.initialNode.value
+        break
+    }
+
+    console.log(`\n=>> Iniciando o round ${round}.\n`)
+
+    
+    console.log(`=>> O jogador atual é o ${currentPlayer.value}`)
+    
+    let jogada = readline.question('=>>Pressione enter para selecionar uma carta!')
+    const number = getRandomNumber()
+
+    console.log(`=>> Foi tirada a carta ${number}`)
+
+    const auxCurrentPlayer = { ...currentPlayer }
+
+    switch (number) {
+        case 0:
+            currentPlayer = executeToZeroNumber()
+            break
+
+        case 1:
+            currentPlayer = executeToOneNumber(currentPlayer)
+            break
+
+        case 3:
+            currentPlayer = executeToThreeNumber(linkedList, currentPlayer)
+            break
+
+        case 9:
+            currentPlayer = executeToNineNumber(linkedList, currentPlayer)
+            break
+
+        case 12:
+            if (round === 1) {
+                console.log('=>> Pulando para o próximo jogador, pois é a primeira rodada')
+                currentPlayer = currentPlayer.nextNode
+                break
+            }
+
+            if ([3].includes(lastNumber)) {
+                console.log('=>> Pulando para o próximo player pois a carta anterior é a 3, que já eliminou o jogador anterior')
+                currentPlayer = currentPlayer.nextNode
+                break
+            }
+            currentPlayer = executeToTwelveNumber(linkedList, currentPlayer, lastPlayer)
+            break
+
+        case 13:
+            currentPlayer = executeToThirteenNumber()
+            break
+    }
+
+    lastPlayer = { ...auxCurrentPlayer }
+    lastNumber = number
+
+    console.log(`=>> Passando para o player ${currentPlayer.value}`)
+
+    round++
 }
+
+console.log('\n=>>>> Mostrando o vencedor...')
+console.log(`\n=>>>> O vencedor ${winner} é implacável`)
+
+}
+
+main()
