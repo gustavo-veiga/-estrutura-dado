a61188811540034047a76071cb67f9c91432fb73
Gustavo Veiga <gustavomorenodaveiga@live.com>
2020-05-04 22:28:49 -0300

    cpp_src: std::cout to fmt::print

--- cpp_src/list/source/menu.cc
+++ cpp_src/list/source/menu.cc
@@ -4,23 +4,21 @@
 #include <lesson/console.h>
 #include <lesson/search.h>
 #include <lesson/sort.h>
+#include <fmt/format.h>
 #include <iostream>
 
 namespace list {
 int menu::show() {
   int choise;
-  for (int i = 1; i < lesson::console::columns(); i++) {
-    std::cout << "-";
-  }
-  std::cout << std::endl;
+  fmt::print("{0:─^{1}}", "", lesson::console::columns());
   std::cout << "1 - Exibir o vetor" << std::endl;
   std::cout << "2 - Ordenar os elementos (Quick Sort)" << std::endl;
   std::cout << "3 - Ordenar os elementos (Bubble Sort)" << std::endl;
   std::cout << "4 - Realizar a Busca Linear por um elemento" << std::endl;
   std::cout << "5 - Realizar a Busca Binaria por um elemento" << std::endl;
   std::cout << "6 - Excluir um elemento" << std::endl;
-  std::cout << "7 - Encerar" << std::endl;
-  std::cout << "Selecione uma opcao [1-7]: ";
+  std::cout << "7 - Encerrar" << std::endl;
+  fmt::print("Selecione uma opção [1-7]: ");
   std::cin >> choise;
   return choise;
 }
@@ -35,57 +33,57 @@ void menu::show_all(lesson::array::vector<int> &vec) {
 
 void menu::linear_search(lesson::array::vector<int> &vec) {
   int search;
-  std::cout << "Insira o elemento a ser buscado: ";
+  fmt::print("Insira o elemento a ser buscado: ");
   std::cin >> search;
   int result = lesson::search::linear(vec, search);
   if (result >= 0) {
-    std::cout << "Elemento encontrado na posicao  " << result << std::endl;
+    fmt::print("Elemento encontrado na posição {}.\n", result);
   } else {
-    std::cout << "Elemento nao encontrado" << std::endl;
+    fmt::print("Elemento não encontrado.\n");
   }
 }
 
 void menu::binary_search(lesson::array::vector<int> &vec) {
   int search;
   int result;
-  std::cout << "Insira o elemento a ser buscado: ";
+  fmt::print("Insira o elemento a ser buscado: ");
   std::cin >> search;
   if (!vec.is_sorted) {
     lesson::sort::quick(vec);
   }
   result = lesson::search::binary(vec, search);
   if (result >= 0) {
-    std::cout << "Elemento encontrado na posicao " << result << std::endl;
+    fmt::print("Elemento encontrado na posição {}.\n", result);
   } else {
-    std::cout << "Elemento nao encontrado" << std::endl;
+    fmt::print("Elemento não encontrado.\n");
   }
 }
 
 void menu::bubble_sort(lesson::array::vector<int> &vec) {
   double time = lesson::chrono::duration([&vec]() { lesson::sort::bubble(vec); });
-  std::cout << "Os elementos foram ordenados em um tempo de " << time << " segundos." << std::endl;
+  fmt::print("Os elementos foram ordenados em um tempo de {} segundos.\n", time);
   menu::show_all(vec);
 }
 
 void menu::quick_sort(lesson::array::vector<int> &vec) {
   double time = lesson::chrono::duration([&vec]() { lesson::sort::quick(vec); });
-  std::cout << "Os elementos foram ordenados em um tempo de " << time << " segundos." << std::endl;
+  fmt::print("Os elementos foram ordenados em um tempo de {} segundos.\n", time);
   menu::show_all(vec);
 }
 
 void menu::remove_element(lesson::array::vector<int> &vec) {
   int index;
-  std::cout << "Insira o indice do elemento a ser removido: ";
+  fmt::print("Insira o índice do elemento a ser removido: ");
   std::cin >> index;
   vec.remove(index);
 }
 
 void menu::exit() {
-  std::cout << "Encerrando a aplicacao..." << std::endl;
+  fmt::print("Encerrando a aplicação...\n");
   std::exit(EXIT_SUCCESS);
 }
 
 void menu::invalid() {
-  std::cout << "Opcao invalida" << std::endl;
+  fmt::print("Opção inválida.\n");
 }
 }  // namespace list
