ac06459283a8712af2d8fd22d0b8adb469a306d5
Gustavo Veiga <gustavomorenodaveiga@live.com>
2020-05-06 12:26:12 -0300

    cpp_src: use Catch2 for tests

--- cpp_src/lesson/test/vector.cc
+++ cpp_src/lesson/test/vector.cc
@@ -1,7 +1,23 @@
+#define CATCH_CONFIG_MAIN
 #include <lesson/array/vector.h>
-#include <gtest/gtest.h>
 
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+#include <catch2/catch.hpp>
+
+SCENARIO("vectors can be sized and resized", "[vector]") {
+  GIVEN("A vector with some items") {
+    auto v = lesson::array::vector<int>(5);
+
+    REQUIRE(v.size() == 0);
+    REQUIRE(v.capacity() >= 5);
+
+    WHEN("the add is increased") {
+      for (size_t i = 0; i < 10; i++) {
+        v.push(i);
+      }
+      THEN("the size and capacity change") {
+        REQUIRE(v.size() == 10);
+        REQUIRE(v.capacity() >= 10);
+      }
+    }
+  }
 }
