ec5083b5e34f1fa91368c0a4266cf64dd56bb390
Gustavo Veiga <gustavomorenodaveiga@live.com>
2020-05-24 18:38:50 -0300

    javascript_src: QueuePriority extends Queue Class

--- javascript_src/src/lesson/queue-priority.js
+++ javascript_src/src/lesson/queue-priority.js
@@ -1,44 +1,4 @@
-class QueuePriority {
-  constructor(size) {
-    this.elements = new Array(size);
-    this.total = 0;
-    this.first = 0;
-    this.last = 0;
-  }
-
-  // Element access
-  get front() {
-    return this.elements[this.last];
-  }
-
-  get back() {
-    return this.elements[this.last];
-  }
-
-  // Capacity
-  get size() {
-    return this.total;
-  }
-
-  isEmpty() {
-    return this.total == this.elements.length;
-  }
-
-  isFull() {
-    return this.total == 0;
-  }
-
-  // Modifiers
-  pop() {
-    if(this.isEmpty()) {
-      return undefined;
-    }
-    let element = this.elements[this.first];
-    this.first = (this.first + 1) % this.elements.length;
-    this.total--;
-    return element;
-  }
-
+class QueuePriority extends Queue {
   push(element, priority) {
     const data = { element, priority };
     this.elements[this.last] = data;
